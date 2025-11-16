a0c2f98046d1ac0cc9477cd90842208fdda50339
nurnewas <nurnewasjr@gmail.com>
2025-11-16 12:52:03 -0600

    tuple

--- src/non-primative.ts
+++ src/non-primative.ts
@@ -4,9 +4,17 @@ let bazarList: string[] = ['rice', 'oil', 'vegetables']; // array
 bazarList.push('salt');
 // bazarList.push(11) //! error
 
-let mixArr : (string | number | boolean)[] = [11, 'apple', true ]; // mixed array
+let mixArr: (string | number | boolean)[] = [11, 'apple', true]; // mixed array
 
 mixArr.push(11)
 mixArr.push("Nur")
 mixArr.push(false)
 console.log(mixArr);// now i can push string, number, boolean
+
+// todo: Tuple
+let coordinate: [number, number] = [20, 20]
+let couple: [string, string] = ['NurNewas', "who??"] 
+let nameAndAge: [string,number] =["NurNewas", 21]
+
+console.log(coordinate, couple, nameAndAge);
+// nameAndAge.posh(22) // ! error tuple 
