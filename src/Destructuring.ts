9a8b63a0424febbf07f37f13251687a29604c8c9
nurnewas <nurnewasjr@gmail.com>
2025-11-17 22:53:00 +0600

    Destructuring Array and Object

---
+++ src/Destructuring.ts
@@ -0,0 +1,26 @@
+//! Object Destructuring
+
+const user = {
+    id: 1,
+    name: {
+        firstName: "Nur",
+        middleName: "Newas",
+        lastName: "Busuniya"
+    },
+    age: 21,
+    gender: "Male"
+}
+// const myGender = user.gender;
+// const middleName = user.name.middleName;
+const { middleName: myMiddleName } = user.name;
+const { name: { lastName: myLastName } } = user;
+const { gender: myGenderIs } = user;
+
+// console.log(myMiddleName, myGenderIs,myLastName);
+
+//! Array Destructuring
+const myFriends: string[] = ["Arafat", "Rafi", "Shuvo", "Sakib", "Rony"];
+// const myBestFriends= myFriends[1];
+
+const [,myBestFriends] = myFriends;
+console.log(myBestFriends);
