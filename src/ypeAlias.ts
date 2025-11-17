8e551de726fc00641254debf79d778b66774fa9f
nurnewas <nurnewasjr@gmail.com>
2025-11-17 23:11:46 +0600

    Type Alias  ==> No, repetitive code so it's Look prity

---
+++ src/ypeAlias.ts
@@ -0,0 +1,96 @@
+// ! Type Alias 
+
+const user1: {
+    id: number,
+    name: {
+        firstName: string;
+        middleName: string;
+        lastName: string;
+    },
+    age: number,
+    gender: "Male" | "Female",
+    addArress: {
+        division: string;
+
+        City: string;
+    }
+} = {
+    id: 1,
+    name: {
+        firstName: "Nur",
+        middleName: "Newas",
+        lastName: "Busuniya"
+    },
+    age: 21,
+    gender: "Male",
+    addArress: {
+        division: "Dinajpur",
+        City: "Dinajpur Sadar"
+    }
+}
+
+//user 2
+const user2: {
+    id: number,
+    name: {
+        firstName: string;
+        middleName: string;
+        lastName: string;
+    },
+    age: number,
+    gender: "Male" | "Female",
+    addArress: {
+        division: string;
+
+        City: string;
+    }
+} = {
+    id: 2,
+    name: {
+        firstName: "X",
+        middleName: "Y",
+        lastName: "Z"
+    },
+    age: 22,
+    gender: "Female",
+    addArress: {
+        division: "A",
+        City: "B Sadar"
+    }
+}
+
+// I don't like this repetitive code
+// So, I will use Type Alias
+
+type UserType = {
+    id: number,
+    name: {
+        firstName: string;
+        middleName: string;
+        lastName: string;
+    },
+    age: number,
+    gender: "Male" | "Female",
+    addArress: {
+        division: string;
+
+        City: string;
+    }}
+
+    const user3: UserType ={
+    id: 3
+    
+    ,
+    name: {
+        firstName: "NN",
+        middleName: "JJ",
+        lastName: "JJJ"
+    },
+    age: 22,
+    gender: "Female",
+    addArress: {
+        division: "AXX",
+        City: "Y Sadar"
+    }
+}
+console.log(user3);
