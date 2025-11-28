932b094873a88453c5c3c0157d185e37296ad368
nurnewas <nurnewasjr@gmail.com>
2025-11-28 17:28:08 +0600

    Constraint

--- src/Constraint.ts
+++ src/Constraint.ts
@@ -1,5 +1,11 @@
 
-const addStudentToTheCourse = <T extends { id: number, name: string }>(studentInfo: T) => {
+
+type StudentMustHave = {
+    id: number,
+    name: string,
+    class: string
+}
+const addStudentToTheCourse = <T extends StudentMustHave>(studentInfo: T) => {
     return {
         course: "TypeScript Basics",
         ...studentInfo
@@ -10,17 +16,24 @@ const student1 = {
     id: 101,
     name: "Alice",
     age: 20,
-    grade: "A"
+    grade: "A",
+    class: "10th Grade"
 
 }
 const student2 = {
     id: 102,
     name: "Bob",
     age: 22,
-    grade: "B"
+    grade: "B",
+    class: "12th Grade"
 }
 const student3 = {
+    name: "Charlie",
+    age: 19,
+    grade: "C",
+    class: "11th Grade",
     hasAStudentID: true,
+
 }
 
 console.log(addStudentToTheCourse(student1));
